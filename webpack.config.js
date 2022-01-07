99d94d00da12293093a934649b3d8b0324bc9be4
momomox <moroto012@gmail.com>
2022-01-07 16:47:19 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    mode: "development",
+
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    entry: `./src/index.js`,
+  
+    // ファイルの出力設定
+    output: {
+      //  出力ファイルのディレクトリ名
+      path: `${__dirname}/dist`,
+      // 出力ファイル名
+      filename: "main.js"
+    },
+
+    devServer: {
+        static: "./",
+        open: true
+    }
+};
